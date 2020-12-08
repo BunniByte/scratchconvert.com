d6b0ad41c1e38716fa7f6fccc929667c93538b08
SheepDog <henryrichhart@gmail.com>
2020-12-07 19:56:32 -0500

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,18 +1,18 @@
 module.exports = {
-    siteUrl: `https://blog.akansh.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://scratchconvert.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Gatsby Theme Ghost`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Minimalistic Gatsby theme for blog`, // This allows an alternative site description for meta data for pages.
-    siteTwitterHandle: `@akanshgulati`,
-    siteFacebookHandle: `akanshgulati`,
+    siteTitleMeta: `ScratchConvert`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `All about how to convert Scratch projects to different platforms!`, // This allows an alternative site description for meta data for pages.
+    siteTwitterHandle: `@bytebunni`,
+    siteFacebookHandle: `bytebunni`,
     language: `en`,
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Personal Blog`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `ScratchConvert`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
